b143b0543d26ef580c0df6ac9d6b518f3b77dac6
Rubens Takiguti Ribeiro <rubens.ribeiro@printi.com.br>
2020-06-18 21:26:34 -0300

    Use uuid

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const uuid = require('uuid');
 
 const db = [];
-let lastId = 0;
 
 /****************************************************
  * API REST (Back-end)
@@ -10,24 +10,13 @@ let lastId = 0;
 const backendApp = express();
 backendApp.use(bodyParser.json());
 
-backendApp.post('/v1/books', (req, res, next) => {
-  lastId += 1;
-  const entity = {
-    id: lastId,
-    ...req.body,
-  };
-
-  db.push(entity);
-
-  res.status(201).json(entity);
-  next();
-});
-
+// GET ALL
 backendApp.get('/v1/books', (req, res, next) => {
   res.status(200).json({ books: db });
   next();
 });
 
+// GET ONE
 backendApp.get('/v1/books/:id', (req, res, next) => {
   const entity = db.find(book => book.id == req.params.id);
 
@@ -41,12 +30,26 @@ backendApp.get('/v1/books/:id', (req, res, next) => {
   next();
 });
 
+// CREATE
+backendApp.post('/v1/books', (req, res, next) => {
+  const entity = {
+    id: uuid.v4(),
+    ...req.body,
+  };
+
+  db.push(entity);
+
+  res.status(201).location(`/v1/${entity.id}`).json(entity);
+  next();
+});
+
+// REPLACE
 backendApp.put('/v1/books/:id', (req, res, next) => {
   const i = db.findIndex(book => book.id == req.params.id);
 
   if (i < 0) {
     const entity = {
-      id: parseInt(req.params.id, 10),
+      id: req.params.id,
       ...req.body,
     };
 
@@ -65,6 +68,27 @@ backendApp.put('/v1/books/:id', (req, res, next) => {
   next();
 });
 
+// UPDATE
+backendApp.patch('/v1/books/:id', (req, res, next) => {
+  const i = db.findIndex(book => book.id == req.params.id);
+
+  if (i < 0) {
+    res.status(404).json({ message: 'book not found' });
+    next();
+    return;
+  }
+
+  const entity = {
+    ...req.body,
+    id: db[i].id,
+  };
+  db[i] = entity;
+
+  res.status(200).json(db[i]);
+  next();
+});
+
+// DELETE
 backendApp.delete('/v1/books/:id', (req, res, next) => {
   const i = db.findIndex(book => book.id == req.params.id);
 
